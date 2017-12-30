f52dced8ae4ce7b1480dcdc73b1ef78d2115ec24
dimitris <dlachanas@gmail.com>
2017-12-30 15:44:54 +0100

    converts ui classes to kotlin

---
+++ app/src/main/java/com/fishbrain/assignment/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.fishbrain.assignment
+
+import android.os.Bundle
+import android.widget.Toast
+
+import com.fishbrain.android.event.CatchesEvent
+import com.fishbrain.android.network.RetrofitInstance
+import com.fishbrain.android.network.interactor.CatchesInteractor
+import com.fishbrain.android.network.interactor.CatchesInteractorImpl
+import com.fishbrain.android.network.service.ApiService
+import com.fishbrain.assignment.base.BaseActivity
+
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+
+import java.io.IOException
+
+/**
+ * Created by dimitris on 19/07/16.
+ */
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        EventBus.getDefault().register(this)
+        val catchesInteractor = CatchesInteractorImpl(RetrofitInstance.instance!!.create(ApiService::class.java))
+        try {
+            catchesInteractor.fetchCatches("1")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onDestroy() {
+        EventBus.getDefault().unregister(this)
+        super.onDestroy()
+    }
+
+    @Subscribe
+    fun onEvent(event: CatchesEvent) {
+        Toast.makeText(this@MainActivity, event.data!!.size.toString() + " catches fetched!", Toast.LENGTH_LONG).show()
+    }
+}
